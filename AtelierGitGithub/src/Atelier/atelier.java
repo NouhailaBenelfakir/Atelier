f3b531d66c6c18f04b392168bc93acc13546dee7
Nouhaila-Ben-Elfakir <nouhailabenelfakir02@gmail.com>
2024-01-19 10:26:28 +0100

    initial commit

---
+++ AtelierGitGithub/src/Atelier/atelier.java
@@ -0,0 +1,7 @@
+package Atelier;
+
+public class atelier {
+	public static void main(String[] args) { 
+	System.out.println("Bonjour ,bienvenue dans notre premier atelier :Processus Git et GitHub sous eclipse");
+	}
+}
