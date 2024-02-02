fc8291ad77722f6a522f63cd8d240e2e7dd50fda
Nouhaila-Ben-Elfakir <nouhailabenelfakir02@gmail.com>
2024-02-02 21:27:27 +0100

    another commit

--- AtelierGitGithub/src/Atelier/atelier.java
+++ AtelierGitGithub/src/Atelier/atelier.java
@@ -3,5 +3,10 @@ package Atelier;
 public class atelier {
 	public static void main(String[] args) { 
 	System.out.println("Bonjour ,bienvenue dans notre premier atelier :Processus Git et GitHub sous eclipse");
+	String developpeur = "Je suis le développeur.";
+    System.out.println(developpeur);
+
+     // Modification de la salutation
+     System.out.println("Merci de participer à cet atelier Git et GitHub!");
 	}
 }
