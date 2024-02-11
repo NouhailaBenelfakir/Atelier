a8cea49672c1aaca59b67c87e7fe1030fe4034db
Nouhaila-Ben-Elfakir <nouhailabenelfakir02@gmail.com>
2024-02-11 12:22:12 +0100

    this is a rebase

--- AtelierGitGithub/src/Atelier/atelier.java
+++ AtelierGitGithub/src/Atelier/atelier.java
@@ -5,6 +5,7 @@ public class atelier {
 	System.out.println("Bonjour ,bienvenue dans notre premier atelier :Processus Git et GitHub sous eclipse");
 	String developpeur = "Je suis le développeur.";
     System.out.println(developpeur);
+    System.out.println("rebase");
 
      // Modification de la salutation
      System.out.println("Merci de participer à cet atelier Git et GitHub!");
